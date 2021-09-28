d62d1636a8f899d49dc928207af564f94742f38b
vidhik2002 <vidhik2002@gmail.com>
2021-09-28 16:51:35 +0530

    feat: leaderboard route

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+require("dotenv/config");
+require("./models/dbInit");
+
+app.set("trust proxy", true);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const scoreRoute = require("./routes/leaderboard");
+app.use("/score", scoreRoute);
+
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
+
+app.listen(3001, () => {
+  console.log("server started");
+});
